f02fca6038e0a13d4f8050133b0f459f61249c1d
Luciano Palma <lucianopalma90@gmail.com>
2017-07-19 16:54:16 +0100

    should throw expection if unable to connect to client

--- src/test/java/com/codurance/telemetrySystemKata/TelemetryDiagnosticControlsShould.java
+++ src/test/java/com/codurance/telemetrySystemKata/TelemetryDiagnosticControlsShould.java
@@ -1,5 +1,6 @@
 package com.codurance.telemetrySystemKata;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InOrder;
@@ -10,17 +11,21 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.inOrder;
 
 @RunWith(MockitoJUnitRunner.class)
-public class TelemetryDiagnosticControlsShould
-{
+public class TelemetryDiagnosticControlsShould {
 
+    public static final boolean DISCONNECTED = false;
     @Mock
     private TelemetryClient telemetryClient;
+    private TelemetryDiagnosticControls telemetryDiagnosticControls;
+
+    @Before
+    public void setUp() throws Exception {
+        telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClient);
+    }
 
     @Test
     public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
         given(telemetryClient.getOnlineStatus()).willReturn(true);
-
-        TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClient);
 
         telemetryDiagnosticControls.checkTransmission();
 
@@ -29,4 +34,10 @@ public class TelemetryDiagnosticControlsShould
         inOrder.verify(telemetryClient).receive();
     }
 
+    @Test(expected = Exception.class)
+    public void throw_exception_if_unable_to_connect_to_client() throws Exception {
+        given(telemetryClient.getOnlineStatus()).willReturn(DISCONNECTED);
+
+        telemetryDiagnosticControls.checkTransmission();
+    }
 }
