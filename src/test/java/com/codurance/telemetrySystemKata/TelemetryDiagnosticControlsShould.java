ae0e011b296f18a4fcb8c0cce83b9079805beabd
Luciano Palma <lucianopalma90@gmail.com>
2017-07-19 16:40:31 +0100

    overload constructor to inject TelemetryClient

--- src/test/java/com/codurance/telemetrySystemKata/TelemetryDiagnosticControlsShould.java
+++ src/test/java/com/codurance/telemetrySystemKata/TelemetryDiagnosticControlsShould.java
@@ -1,13 +1,32 @@
 package com.codurance.telemetrySystemKata;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.inOrder;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TelemetryDiagnosticControlsShould
 {
-    
-	@Test
-    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
-    {
+
+    @Mock
+    private TelemetryClient telemetryClient;
+
+    @Test
+    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
+        given(telemetryClient.getOnlineStatus()).willReturn(true);
+
+        TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemetryClient);
+
+        telemetryDiagnosticControls.checkTransmission();
+
+        InOrder inOrder = inOrder(telemetryClient);
+        inOrder.verify(telemetryClient).send(TelemetryClient.DIAGNOSTIC_MESSAGE);
+        inOrder.verify(telemetryClient).receive();
     }
 
 }
