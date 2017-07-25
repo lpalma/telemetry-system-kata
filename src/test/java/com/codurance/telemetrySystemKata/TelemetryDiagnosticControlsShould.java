68824710cc3129f249eb7f290c0009748d341384
Luciano Palma <lucianopalma90@gmail.com>
2017-07-25 14:47:19 +0100

    rename test

--- src/test/java/com/codurance/telemetrySystemKata/TelemetryDiagnosticControlsShould.java
+++ src/test/java/com/codurance/telemetrySystemKata/TelemetryDiagnosticControlsShould.java
@@ -27,7 +27,7 @@ public class TelemetryDiagnosticControlsShould {
     }
 
     @Test
-    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
+    public void send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
         given(telemetryClient.getOnlineStatus()).willReturn(CONNECTED);
 
         telemetryDiagnosticControls.checkTransmission();
